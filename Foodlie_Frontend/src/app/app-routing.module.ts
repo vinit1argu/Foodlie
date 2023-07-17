cae5874c8b635874cbf73db65c5aa40a75451f0b
Vinit Shukla <123145108+vinit1argu@users.noreply.github.com>
2023-07-17 17:48:33 +0530

    added restaurant component

--- Foodlie_Frontend/src/app/app-routing.module.ts
+++ Foodlie_Frontend/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomepageComponent } from './homepage/homepage.component';
-
+import { RestaurantComponent } from './component/restaurant/restaurant.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'restaurants', component: RestaurantComponent}
 ];
 
 @NgModule({
